43c3b51eb8625d36e85bb6bbf0346e857ae504e4
Tawanjot Singh <54388983+tawan21@users.noreply.github.com>
2022-08-26 17:31:17 +0530

    Header a bit more responsive!

--- components/Header.js
+++ components/Header.js
@@ -5,28 +5,32 @@ import { IconButton } from '@mui/material';
 function Header() {
     return (
         <header className="sticky top-0 z-50 flex items-center p-3 shadow-md bg-white">
-            <IconButton
-                className="md:inline-flex border-0"
-            >
-                <Icon>menu</Icon>
-            </IconButton>
-            <Icon className="text-blue-600 text-5xl">description</Icon>
+            <div className="flex justify-center">
+                <IconButton
+                    className="md:inline-flex border-0"
+                >
+                    <Icon>menu</Icon>
+                </IconButton>
+                <Icon className="text-blue-600 text-3xl md:text-5xl">description</Icon>
+            </div>
             <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
-            <div className="mx-5 md:mx-20 flex flex-grow items-center p-3 bg-gray-200 text-gray-600 rounded focus-within:shadow focus-within:text-gray-700">
+            <div className="mx-2 md:mx-20 flex flex-grow items-center p-3 bg-gray-200 text-gray-600 rounded focus-within:shadow focus-within:text-gray-700">
                 <Icon className="text-gray-600">search</Icon>
                 <input type="text" placeholder="Search" className="flex-grow px-5 bg-transparent text-base outline-none" />
             </div>
-            <IconButton
-                className="hidden md:inline-flex border-0"
-            >
-                <Icon>apps</Icon>
-            </IconButton>
-            <img
-                loading="lazy"
-                className="cursor-pointer h-10 w-10 rounded-full"
-                src="https://visualpharm.com/assets/30/User-595b40b85ba036ed117da56f.svg"
-                alt=""
-            />
+            <div className="flex justify-center">
+                <IconButton
+                    className="hidden md:inline-flex border-0"
+                >
+                    <Icon>apps</Icon>
+                </IconButton>
+                <img
+                    loading="lazy"
+                    className="cursor-pointer h-10 w-10 rounded-full"
+                    src="https://visualpharm.com/assets/30/User-595b40b85ba036ed117da56f.svg"
+                    alt=""
+                />
+            </div>
         </header>
     )
 }
